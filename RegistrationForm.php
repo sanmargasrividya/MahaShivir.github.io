6518e4656d40332f8cf02c32fd9dc2f16e809756
sanmargasrividya <sindhurabandaru@gmail.com>
2017-10-20 19:11:10 +0530

    Update RegistrationForm.php

--- RegistrationForm.php
+++ RegistrationForm.php
@@ -1,40 +1,86 @@
 <?php
+    phpinfo();
 if(!isset($_POST['submit']))
 {
 	//This page should not be accessed directly. Need to submit the form.
 	echo "error; you need to submit the form!";
 }
-/*$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+$noOfParticipents = $_POST['NumberofParticipants'];
 
 //Validate first
-if(empty($name)||empty($visitor_email)) 
-{
-    echo "Name and email are mandatory!";
-    exit;
-}
+$to = 'info@siddhasanmarga.com';
+	$subject = 'Sanmarga Supramental Healing';
+	$msg = "<html>
+	 <head>
+	 <title>Title of email</title>
+	 </head>	
+	 <body>";
+	 
+$tableHead = 
+	 "<table cellspacing=\"4\" cellpadding=\"4\" border=\"1\" align=\"center\">	
+		<tr>
+		<td align=\"center\">Name</td>
+		<td align=\"center\">Location</td>
+		<td align=\"center\">Email</td>
+		<td align=\"center\">Phone Number</td>
+		</tr>";
+	
+	for( $i = 0; $i<$noOfParticipents; $i++ ) {		
+		$tableData = $tableHead +
+			"<td align=\"center\">".$_POST['name'][i]."</td>
+			<td align=\"center\">".$_POST['location'][i]."</td>
+			<td align=\"center\">".$_POST['email'][i]."</td>
+			<td align=\"center\">".$_POST['phonenumber'][i]."</td>
+			</tr>"	;
+	}
+	
+	$msg = $msg + $tableHead +"</table>
+	</body>
+	</html>";
 
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
-*/
-$email_from = 'sindhura@hawkeslearning.com';//<== update the email address
+    /*if(empty($_POST['name'][i])) 
+	{
+	    echo "Name is mandatory!";
+	    exit;
+	}
+	if(empty($_POST['location'][i])){
+		echo "Location is mandatory!";
+	    exit;
+	}	
+	if(empty($_POST['phonenumber'][i])){
+		 echo "Bad email value!";
+	    exit;
+	}
+	if(IsInjected(empty($_POST['email'][i])))
+	{
+	    echo "Bad email value!";
+	    exit;
+	}
+	*/
+	
+      
+	  // Make sure to escape quotes
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: siddhasanmarga.org <info@siddhasanmarga.org>' . "\r\n";
+
+mail($to, $subject, $msg, $headers);
+
+   
+/*$email_from = 'sindhurabandaru@gmail.com';//<== update the email address
 $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
-    "Here is the message:\n $message".
+    "Here is the message:\n".// $message".
     
-$to = 'sindhura@hawkeslearning.com';//<== update the email address
-$headers = "From: $sindhura@hawkeslearning.com \r\n";
-$headers .= "Reply-To: $sindhura@hawkeslearning.com \r\n";
+$to = "sindhurabandaru@gmail.com";//<== update the email address
+$headers = "From: $sindhurabandaru@gmail.com \r\n";
+$headers .= "Reply-To: $sindhurabandaru@gmail.com \r\n";
 //Send the email!
-mail($to,$email_subject,$email_body,$headers, "-f your@email.here");
+mail($to,$email_subject,$email_body,$headers);
 //done. redirect to thank-you page.
-header('Location: thank-you.html');
-
-
+//header('Location: thank-you.html');
+ */
 // Function to validate against any email injection attempts
 function IsInjected($str)
 {
@@ -57,5 +103,5 @@ function IsInjected($str)
     return false;
   }
 }
-   
-?> 
+
+?>
